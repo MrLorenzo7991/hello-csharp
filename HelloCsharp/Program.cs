a638bb39a5d2d99cf4cd158d9964c24656bd7aad
Lorenzo <lorenzo.miccio97@gmail.com>
2022-03-31 14:06:57 +0200

    Completato primo programma

--- HelloCsharp/Program.cs
+++ HelloCsharp/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Benvenuto
+Console.WriteLine("Benvenuto nel mio primo programma C#!");
+Console.WriteLine("Questi sono i mesi dell'anno: ");
+
+//Mesi dell'anno
+Console.WriteLine("Gennaio");
+Console.WriteLine("Febbraio");
+Console.WriteLine("Marzo");
+Console.WriteLine("Aprile");
+Console.WriteLine("Maggio");
+Console.WriteLine("Giugno");
+Console.WriteLine("Luglio");
+Console.WriteLine("Agosto");
+Console.WriteLine("Settembre");
+Console.WriteLine("Ottobre");
+Console.WriteLine("Novembre");
+Console.WriteLine("Dicembre");
+
+//Domanda
+Console.WriteLine("Scrivi il tuo mese preferito: ");
+var mese = Console.ReadLine();
+Console.WriteLine("Davvero? Anche il mio mese preferito è " + mese + "!");
+Console.WriteLine("Grazie ed arrivederci!");
